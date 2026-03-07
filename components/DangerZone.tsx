21d6f2c86aa37714e14e11f55599e8e927d25caf
Mehedi Hasan <thevain4216@gmail.com>
2026-03-07 12:16:22 +0600

    App Done

---
+++ components/DangerZone.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DangerZone = () => {
+  return (
+    <View>
+      <Text>DangerZone</Text>
+    </View>
+  )
+}
+
+export default DangerZone
---
+++ components/Preferences.tsx
@@ -0,0 +1,66 @@
+import { createSettingsStyles } from "@/assets/images/styles/settings.styles";
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from "react";
+import { Switch, Text, View } from "react-native";
+
+const Preferences = () => {
+  const [isAutoSync, setIsAutoSync] = useState(true);
+  const [isNotificationsEnabled, setIsNotificationsEnabled] = useState(true);
+
+  const { isDarkMode, toggleDarkMode, colors } = useTheme();
+
+  const settingsStyles = createSettingsStyles(colors);
+
+  return (
+    <LinearGradient
+      colors={colors.gradients.surface}
+      style={settingsStyles.section}
+    >
+      <Text style={settingsStyles.sectionTitle}>Preferences</Text>
+      {/* DARK MODE */}
+      <View style={settingsStyles.settingItem}>
+        <View style={settingsStyles.settingLeft}>
+          <LinearGradient
+            colors={colors.gradients.primary}
+            style={settingsStyles.settingIcon}
+          >
+            <Ionicons name="moon" size={18} color="#fff" />
+          </LinearGradient>
+          <Text style={settingsStyles.settingText}>Dark Mode</Text>
+        </View>
+        <Switch
+          value={isDarkMode}
+          onValueChange={toggleDarkMode}
+          thumbColor={"#fff"}
+          trackColor={{ false: colors.border, true: colors.primary }}
+        //   for IOS background color of the switch track
+        //   ios_backgroundColor={colors.border}
+        />
+      </View>
+
+       <View style={settingsStyles.settingItem}>
+        <View style={settingsStyles.settingLeft}>
+          <LinearGradient
+            colors={colors.gradients.warning}
+            style={settingsStyles.settingIcon}
+          >
+            <Ionicons name="notifications" size={18} color="#fff" />
+          </LinearGradient>
+          <Text style={settingsStyles.settingText}>Notifications</Text>
+        </View>
+        <Switch
+          value={isNotificationsEnabled}
+          onValueChange={() => setIsNotificationsEnabled(!isNotificationsEnabled)}
+          thumbColor={"#fff"}
+          trackColor={{ false: colors.border, true: colors.warning }}
+        //   for IOS background color of the switch track
+        //   ios_backgroundColor={colors.border}
+        />
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default Preferences;
